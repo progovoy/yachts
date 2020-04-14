5eb06c8699bcb8c415006ba6cbfe90805121e740
Pavel Rogovoy <progovoy@gmail.com>
2020-04-15 02:52:43 +0300

    add support fot the other 2 theories

--- exam.py
+++ exam.py
@@ -1,31 +1,44 @@
 import random
 import pickle
+import os
 
 
 def init_game():
-    with open('answers.txt', 'r') as f:
+    with open(f'a_{exam_name}.txt', 'r') as f:
         ans = f.read()
 
     answers = {}
-    sum = [1, 46, 91, 136]
-    num = [0, 0, 0, 0]
-    index = 0
-    for a in ans.split('\n'):
-        for b in a.split()[1::2]:
-            if b == 'א':
-                b = 'a'
-            elif b == 'ב':
-                b = 'b'
-            elif b == 'ג':
-                b = 'c'
-            elif b == 'ד':
-                b = 'd'
+    n = 0
+    if exam_name == 'yam':
+        sums = ((1, 46, 91, 136),)
+    elif exam_name == 'equ':
+        sums = ((1, 31, 61, 91, 121, 151, 181),)
+    elif exam_name == 'yam':
+        sums = ((1, 48, 95, 142, 189, 236, 283),
+                (330, 338, 346, 354, 362)
+               )
+    else:
+        raise RuntimeError(f'Unsupported exam name: {exam_name}')
 
-            answers.update({num[index] + sum[index]: b})
-            num[index] += 1
-            index = (index + 1) % 4
+    for tsum in sums:
+        num = [0] * len(tsum)
+        index = 0
+        for a in ans.split('\n'):
+            for b in a.split()[1::2]:
+                if b == 'א':
+                    b = 'a'
+                elif b == 'ב':
+                    b = 'b'
+                elif b == 'ג':
+                    b = 'c'
+                elif b == 'ד':
+                    b = 'd'
 
-    with open('questions.txt', 'r') as f:
+                answers.update({num[index] + tsum[index]: b})
+                num[index] += 1
+                index = (index + 1) % len(tsum)
+
+    with open(f'q_{exam_name}.txt', 'r') as f:
         q = f.read()
 
     exam = {}
@@ -63,25 +76,30 @@ def init_game():
 
 
 def save(exam, game, answers):
-    with open('/tmp/save_exam.pickle', 'wb') as f:
+    with open(f'/tmp/save_exam_{exam_name}.pickle', 'wb') as f:
         pickle.dump(exam, f)
-    with open('/tmp/save_game.pickle', 'wb') as f:
+    with open(f'/tmp/save_game_{exam_name}.pickle', 'wb') as f:
         pickle.dump(game, f)
-    with open('/tmp/save_answers.pickle', 'wb') as f:
+    with open(f'/tmp/save_answers_{exam_name}.pickle', 'wb') as f:
         pickle.dump(answers, f)
 
 
-init = input("Start from scratch? y / n")
-if init == 'y':
-    init = input("REALLY?! yes i do / n")
-    if init == 'yes i do':
-        init_game()
+exam_name = 'equ'
 
-with open('/tmp/save_exam.pickle', 'rb') as f:
+if os.path.isfile(f'/tmp/save_exam_{exam_name}.pickle'):
+    init = input("Start from scratch? y / n")
+    if init == 'y':
+        init = input("REALLY?! yes i do / n")
+        if init == 'yes i do':
+            init_game()
+else:
+    init_game()
+
+with open(f'/tmp/save_exam_{exam_name}.pickle', 'rb') as f:
     exam = pickle.load(f)
-with open('/tmp/save_game.pickle', 'rb') as f:
+with open(f'/tmp/save_game_{exam_name}.pickle', 'rb') as f:
     game = pickle.load(f)
-with open('/tmp/save_answers.pickle', 'rb') as f:
+with open(f'/tmp/save_answers_{exam_name}.pickle', 'rb') as f:
     answers = pickle.load(f)
 
 num_of_q = len(game.keys())
@@ -91,16 +109,17 @@ correct_live = 0
 for k,v in game.items():
     correct_live += v['sucess_count']
 
+DIFFICULTY = 2
 while game:
     q, val = random.choice(list(exam.items()))
 
-    print('{0} of 2'.format(game[q]['sucess_count']))
+    print('{0} of {1}'.format(game[q]['sucess_count'], DIFFICULTY))
     print('{0} - {1}'.format(q, val['q']))
     for k, opt in val['opts'].items():
         print('{0} - {1}'.format(k, opt))
 
     user_choice = input("")
-    while user_choice == 's' or user_choice == 'save':
+    while user_choice in ['s', 'save', '?', 'help']:
         if user_choice == 's':
             print(
                 'You have finished {0} questions '
@@ -111,7 +130,20 @@ while game:
             print('{0} answered correctly'.format(correct_live))
         elif user_choice == 'save':
             save(exam, game, answers)
+            print('Saved successfully')
+        elif user_choice in ['?', 'help']:
+            print('Commands are:\n'
+                  '?/help - help\n'
+                  's - status\n'
+                  'save - save exam to continue later\n'
+                  'skip - skip question\n'
+                  'exit - exit exam\n'
+            )
 
+        print('{0} of {1}'.format(game[q]['sucess_count'], DIFFICULTY))
+        print('{0} - {1}'.format(q, val['q']))
+        for k, opt in val['opts'].items():
+            print('{0} - {1}'.format(k, opt))
         user_choice = input("")
 
     if user_choice == 'exit':
@@ -127,7 +159,7 @@ while game:
 
         correct_live += 1
         game[q]['sucess_count'] += 1
-        if game[q]['sucess_count'] == 2:
+        if game[q]['sucess_count'] == DIFFICULTY:
             game.pop(q)
             exam.pop(q)
             answers.pop(q)
